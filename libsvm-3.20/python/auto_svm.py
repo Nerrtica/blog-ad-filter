85d1607e8be316e57843a9883f192ad19338e05e
Nerrtica <nerrtica@naver.com>
2015-08-09 02:29:34 +0900

    make_model, predict_label class 1차 구현 완료

--- libsvm-3.20/python/auto_svm.py
+++ libsvm-3.20/python/auto_svm.py
@@ -38,18 +38,101 @@ class performance_measure:
                 j //= 2
             self.makeFile("{0}{1}{2}".format(self.jsonFileName[:-5], i, ".txt"), selectedFeatureList)
 
-    def play (self):
-        self.readFile()
-        self.makeAllCombi()
+    def svmCalculate (self):
         SCCresult = []
         for i in range(1, 2 ** len(self.featureList)):
             print("order", i)
             prob = svm_read_problem("{0}{1}{2}".format(self.jsonFileName[:-5], i, ".txt"))
             result = svm_train(prob[0], prob[1], "-s 3 -t 0 -v 10 -q")
             SCCresult.append(result[1])
+        return SCCresult
+
+    def play (self):
+        self.readFile()
+        self.makeAllCombi()
+        SCCresult = self.svmCalculate()
         print("order", SCCresult.index(max(SCCresult)), "is optimum combination")
 
+# performance_measure class use example
 fileName = "data/data.json"
 featureList = ["frequancy_unigram", "frequancy_bigram", "frequancy_trigram"]
 pm = performance_measure(fileName, featureList)
 pm.play()
+
+
+class make_model:
+    def __init__ (self, jsonFileName, featureList, modelFileName):
+        self.jsonFileName = jsonFileName
+        self.featureList = featureList
+        self.modelFileName = modelFileName
+
+    def readFile (self):
+        f = open(self.jsonFileName, 'r')
+        self.parsed_json = json.loads(f.read())
+        f.close()
+
+    def makeFile (self):
+        f = open("{0}{1}".format(self.jsonFileName[:-5], ".txt"), 'w')
+        for i in range(len(self.parsed_json)):
+            f.write("%d " % i)
+            # label file not exists
+
+            for feature in self.featureList:
+                f.write("%s " % self.parsed_json[i][feature])
+            f.write("\n")
+        f.close()
+
+    def svmTrain (self):
+        prob = svm_read_problem("{0}{1}".format(self.jsonFileName[:-5], ".txt"))
+        model = svm_train(prob[0], prob[1], "-s 3 -t 0 -q")
+        svm_save_model(self.modelFileName, model)
+
+    def play (self):
+        self.readFile()
+        self.makeFile()
+        self.svmTrain()
+
+# make_model class use example
+modelFileName = "data/model.txt"
+mm = make_model(fileName, featureList, modelFileName)
+mm.play()
+
+
+class predict_label:
+    def __init__ (self, jsonFileName, featureList, modelFileName):
+        self.jsonFileName = jsonFileName
+        self.featureList = featureList
+        self.modelFileName = modelFileName
+
+    def readFile (self):
+        f = open(self.jsonFileName, 'r')
+        self.parsed_json = json.loads(f.read())
+        f.close()
+
+    def makeFile (self):
+        f = open("{0}{1}".format(self.jsonFileName[:-5], ".txt"), 'w')
+        for i in range(len(self.parsed_json)):
+            f.write("%d " % i)
+            # label file not exists
+
+            for feature in self.featureList:
+                f.write("%s " % self.parsed_json[i][feature])
+            f.write("\n")
+        f.close()
+
+    def svmPredict (self):
+        prob = svm_read_problem("{0}{1}".format(self.jsonFileName[:-5], ".txt"))
+        model = svm_load_model (self.modelFileName)
+        result = svm_predict(prob[0], prob[1], model, "-q")
+        return result[0]
+
+    def play (self):
+        self.readFile()
+        self.makeFile()
+        result = self.svmPredict()
+        return result
+
+# predict_label class use example
+predictFileName = "data/data.json"
+pl = predict_label(predictFileName, featureList, modelFileName)
+result = pl.play()
