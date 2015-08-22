247fc24ac44551dabb107c24d7603b7352e36ed2
JHJ <gywns2702@gmail.com>
2015-08-22 21:58:04 +0900

    Ajax 방식 적용

--- BlogAdFilter.py
+++ BlogAdFilter.py
@@ -1,8 +1,11 @@
 # -*- encoding:utf8 -*-
 
 # all the imports
-from flask import Flask, request, session, g, redirect
+from flask import Flask, request, session, g, redirect, jsonify
 from flask import url_for, abort, render_template, flash
+
+from sqlite3 import dbapi2 as sqlite3
+from hashlib import md5
 
 import json
 import sys
@@ -28,10 +31,10 @@ PASSWORD = 'admin'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# View function
-@app.route('/')
-def blog_search():
-	return render_template('blog_search.html')
+# # View function
+# @app.route('/')
+# def blog_search():
+# 	return render_template('blog_search.html')
 
 @app.route('/crawling', methods=['GET', 'POST'])
 def crawling():
@@ -59,6 +62,66 @@ def crawling():
 		score = str(predictor.play()[0])
 	return render_template('filter_result.html', url=url, adscore=score)
 	# return data/Creater.createDataSet()\
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+	error = None
+	if request.method == 'POST':
+		user_score = str(request.form['user_score'])
+	if user_score in ["1", "2", "3", "4", "5"]:
+		return '잘못된 데이터 입력'
+	
+
+
+	return render_template('feedback_ok.html', score=score)
+	# return data/Creater.createDataSet()
+
+
+# Ajax Ver
+
+@app.route('/_extractData')
+def extractData():
+	url = request.args.get('url', 0, type=str)
+	if url.startswith('http://') is False:
+		url = 'http://' + url
+	crawler = NaverBlog.post_crawling(url)
+		
+	data = crawler.write_data_as_dict()
+	dataCreater = DataCreater()
+	dataCreater.loadData(data)
+	dataCreater.loadNgramData()
+	dataCreater.postAnalysis()
+	# return dataCreater.createDataSet()
+	data = json.loads(dataCreater.createDataSet())
+	data_0_jsonFormat = str(json.dumps(data[0], ensure_ascii=False, indent=4))
+	return data_0_jsonFormat
+
+@app.route('/_predict')
+def predict():
+	featureList = ["text_len", "include_map", "img_cnt", "sponser", "f_title_unigram", "f_content_unigram", "f_content_bigram", "f_content_trigram", "sentence_cnt", "word_cnt", "wordInSentence_avg", "external_banner_cnt", "comment_cnt", "tag_cnt"]
+	data = request.args.get('data', 0, type=str)
+	data = json.loads(data)
+	data_list = []
+	data_list.append(data)
+	if data_list[0]["label"] == "5":
+		score = "5"
+	else:
+		predictor = predict_label(data_list, featureList)
+		score = str(predictor.play()[0])
+	return score
+
+
+@app.route('/_addToDB')
+def addToDB():
+	data = request.args.get('data', 0, type=str)
+	user_score = request.args.get('user_score', 0, type=str)
+	return "success"
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
 
 if __name__ == '__main__':
 	app.run()
